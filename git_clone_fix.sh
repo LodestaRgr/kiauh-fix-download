3ada4ea21503cdb4972b47fd0fe0fc01e9abdca7
Yuriy Z. <LodestaRgr@yandex.ru>
2025-03-21 15:37:27 +0300

    Update git_clone_fix.sh

--- git_clone_fix.sh
+++ git_clone_fix.sh
@@ -4,40 +4,36 @@ git_clone() {
 
   echo "-- Apply KIAUH Fix Download -- by LodestaRgr --"
 
-  # Уберём ".git" в конце, если оно есть
+  # remove "git" at the end, if there is one
   repo="$(echo "$repo" | sed 's|\.git$||')"
   
-  # Определяем папку на уровень выше
+  # We define the folder to a higher level
   local branch_home
   branch_home="$(dirname "$branch")"
 
-  # Извлекаем имя репозитория (после github.com/...)
+  # Extracting the repository name (after github.com /...)
   local repo_name
   repo_name="$(echo "$repo" | sed -E 's|^https?://github.com/[^/]+/([^/]+).*|\1|')"
 
-  # Определяем основную ветку (main, master или иная)
+  # Defining the main branch (main, master or other)
   local default_branch
   default_branch="$(git ls-remote --symref "$repo" HEAD 2>/dev/null \
                   | grep '^ref: ' \
                   | sed -E 's#^ref: refs/heads/([^[:space:]]+).*#\1#')"
 
-  # Если вдруг default_branch не определился, вернём ошибку
+  # If the default_branch is undecided, we will return an error.
   if [[ -z "$default_branch" ]]; then
-    echo "Не удалось определить основную ветку у репо: $repo"
+    echo "Couldn't identify the main branch of the repo: $repo"
     return 1
   fi
 
-  # Скачиваем архив
+  # Downloading the archive, unzip
   wget -O "${branch}.zip" "${repo}/archive/refs/heads/${default_branch}.zip" || return 1
-
-  # Распаковываем
   unzip -o "${branch}.zip" -d "$branch_home" || return 1
-
-  # Удаляем архив
   rm -f "${branch}.zip" || return 1
 
-  # Переименовываем распакованную папку в нужный путь
-  # (обычно после распаковки получается: <branch_home>/<repo_name>-<default_branch>)
+  # Rename the unpacked folder to the desired path
+  # (usually, after unpacking, it turns out: <branch_home>/<repo_name>-<default_branch>)
   if ! mv -f "${branch_home}/${repo_name}-${default_branch}" "$branch"; then
     return 1
   fi
